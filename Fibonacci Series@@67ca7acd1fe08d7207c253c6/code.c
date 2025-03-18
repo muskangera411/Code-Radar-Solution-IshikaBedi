53ed3b7a8dec2a7f75e24b68e630d1ef2a7b5502
Ishika <ishikavaidya55@gmail.com>
2025-03-18 18:43:10 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 
-// Function to generate Fibonacci series
-void fibonacciSeries(int n){
-    int a=0,b=1,next;
-    printf("Fibonacci series: %d %d,a,b");
+void fibonacciSeries(int n) {
+    int a = 0, b = 1, next;
+    printf("Fibonacci Series: %d %d ", a, b);
 
-    for(int i = 2;i < n;i++){
-        next = a+b;
+    for (int i = 2; i < n; i++) {
+        next = a + b;
         printf("%d ", next);
         a = b;
         b = next;
     }
 }
-int main(){
+
+int main() {  // Ensure this is the ONLY main() function in your file
     int n;
     printf("Enter the number of terms: ");
     scanf("%d", &n);
 
-    if (n < 2){
+    if (n < 2) {
         printf("Please enter a number greater than or equal to 2.");
-
-    }else{
+    } else {
         fibonacciSeries(n);
     }
+
     return 0;
 }
-
